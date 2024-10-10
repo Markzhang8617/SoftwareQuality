734bf3522a9349cf75190aa45d04feea87ac8a37
Markzhang8617 <mzhang8617@conestogac.on.ca>
2024-10-10 19:49:01 -0400

    3 tests for verifying a zero length for one or more sides and 3 tests for verifying an invalid response (other than a zero length)

--- Assignment2/TriangleSolver/TriangleSolverTest/Program.cs
+++ Assignment2/TriangleSolver/TriangleSolverTest/Program.cs
@@ -169,5 +169,106 @@ namespace TriangleSolverTest
             Assert.That(actual, Is.EqualTo(expected));
         }
 
+        // 10. Test for zero length side (1/3)
+        [Test]
+        public void InvalidTriangle_Input0and5and5_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 0;
+            int secondSide = 5;
+            int thirdSide = 5;
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // 11. Test for zero length side (2/3)
+        [Test]
+        public void InvalidTriangle_Input7and0and7_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 7;
+            int secondSide = 0;
+            int thirdSide = 7;
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // 12. Test for zero length side (3/3)
+        [Test]
+        public void InvalidTriangle_Input3and3and0_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 3;
+            int secondSide = 3;
+            int thirdSide = 0;
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // 13. Test for invalid triangle (1/3)
+        [Test]
+        public void InvalidTriangle_Input1and2and3_OutputInvalid()
+        {
+            // Arrange
+            int firstSide = 1;
+            int secondSide = 2;
+            int thirdSide = 3;
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // 14. Test for invalid triangle (2/3)
+        [Test]
+        public void InvalidTriangle_Input5and10and3_OutputInvalid()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 10;
+            int thirdSide = 3;
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // 15. Test for invalid triangle (3/3)
+        [Test]
+        public void InvalidTriangle_Input1and20and5_OutputInvalid()
+        {
+            // Arrange
+            int firstSide = 1;
+            int secondSide = 20;
+            int thirdSide = 5;
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
